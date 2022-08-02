9e348fe342fbd6c42a4d31bdaaddfd38def6c8bf
Diego <Diegocortes079@gmail.com>
2022-08-01 20:49:25 -0500

    Create contract

---
+++ test/Omniverse.js
@@ -0,0 +1,22 @@
+const {
+  time,
+  loadFixture,
+} = require("@nomicfoundation/hardhat-network-helpers")
+const { expect } = require('chai')
+
+describe('Omniverse Contract', () => {
+  const deployContractFixture = async () => {
+    const [wallet, walletTo] = await ethers.getSigners();
+    const Contract = await ethers.getContractFactory('Omniverse')
+    const contract = await Contract.deploy();
+
+    return { contract, wallet, walletTo }
+  }
+
+  describe('Deployment', () => {
+    it('Should set the right owner', async () => {
+      const { contract, wallet } = await loadFixture(deployContractFixture)
+      expect(await contract.owner()).to.equal(wallet.address)
+    })
+  })
+})
